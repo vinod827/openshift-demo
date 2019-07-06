738fe8bac34f283285a87b8e3fbfbf0021f925a5
Vinod Kumar Nair <vinod827@gmail.com>
2019-07-06 19:29:16 +0530

    my second commit

--- src/main/java/com/openshift/controller/OpenShiftController.java
+++ src/main/java/com/openshift/controller/OpenShiftController.java
@@ -1,9 +1,12 @@
 package com.openshift.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OpenShiftController {
+
+    @RequestMapping("/")
     public String defaultCall(){
         return "Hello Kumar, welcome to OpenShift";
     }
